16340e6d7c4c733287f2d991efaaa50f88c1f459
mathcamargo <matheusoabes@gmail.com>
2023-08-13 22:58:37 -0300

    adicionando desafio

---
+++ src/guanabara/boas_vindas.js
@@ -0,0 +1,5 @@
+function welcome(){
+    let nome = prompt("Qual é o seu nome?")
+    let idade = prompt("Olá, " + nome + "! Quantos anos você tem?")
+    alert("Acabei de conhecer " + nome + ", que tem " + idade + " anos de idade!")
+}
